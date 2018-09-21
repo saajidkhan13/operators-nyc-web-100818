fece68e80cd1cbf79071123a54ddc2a88a5b0793
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-21 03:08:32 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+if speed < 40
+  return true
+end
+if speed > 60
+  return true
+end
+else
+  return false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
+
 	
 
 
